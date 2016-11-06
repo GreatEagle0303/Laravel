b5af3f17601a4ee8fcc50836cc0042bab887902a
greateagle0303 <greateagle0303@gmail.com>
2016-11-06 16:07:13 +0000

    Applied fixes from StyleCI

--- src/Grid.php
+++ src/Grid.php
@@ -4,12 +4,12 @@ namespace Encore\Admin;
 
 use Closure;
 use Encore\Admin\Exception\Handle;
+use Encore\Admin\Facades\Admin as AdminManager;
 use Encore\Admin\Grid\Column;
 use Encore\Admin\Grid\Exporter;
 use Encore\Admin\Grid\Filter;
 use Encore\Admin\Grid\Model;
 use Encore\Admin\Grid\Row;
-use Encore\Admin\Pagination\AdminThreePresenter;
 use Illuminate\Database\Eloquent\Model as Eloquent;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -123,6 +123,13 @@ class Grid
     protected $allowActions = true;
 
     /**
+     * Is grid rows orderable.
+     *
+     * @var bool
+     */
+    protected $orderable = false;
+
+    /**
      * @var Exporter
      */
     protected $exporter;
@@ -143,6 +150,16 @@ class Grid
 
         $this->setupFilter();
         $this->setupExporter();
+    }
+
+    /**
+     * Get primary key name of model.
+     *
+     * @return string
+     */
+    public function getKeyName()
+    {
+        return $this->keyName ?: 'id';
     }
 
     /**
@@ -217,7 +234,10 @@ class Grid
     {
         //$label = $label ?: Str::upper($column);
 
-        return $this->columns[] = new Column($column, $label);
+        $column = new Column($column, $label);
+        $column->setGrid($this);
+
+        return $this->columns[] = $column;
     }
 
     public function blank($label)
@@ -256,9 +276,7 @@ class Grid
     {
         $query = Input::all();
 
-        return $this->model()->eloquent()->appends($query)->render(
-            new AdminThreePresenter($this->model()->eloquent())
-        );
+        return $this->model()->eloquent()->appends($query)->render('admin::pagination');
     }
 
     /**
@@ -401,6 +419,28 @@ class Grid
     }
 
     /**
+     * Set grid as orderable.
+     *
+     * @return $this
+     */
+    public function orderable()
+    {
+        $this->orderable = true;
+
+        return $this;
+    }
+
+    /**
+     * Is the grid orderable.
+     *
+     * @return bool
+     */
+    public function isOrderable()
+    {
+        return $this->orderable;
+    }
+
+    /**
      * Set the grid filter.
      *
      * @param callable $callback
@@ -530,12 +570,93 @@ class Grid
     }
 
     /**
+     * Js code for grid.
+     *
+     * @return string
+     */
+    public function script()
+    {
+        $path = app('router')->current()->getPath();
+        $token = csrf_token();
+        $confirm = trans('admin::lang.delete_confirm');
+
+        return <<<EOT
+
+$('.grid-select-all').change(function() {
+    if (this.checked) {
+        $('.grid-item').prop("checked", true);
+    } else {
+        $('.grid-item').prop("checked", false);
+    }
+});
+
+$('.batch-delete').on('click', function() {
+    var selected = [];
+    $('.grid-item:checked').each(function(){
+        selected.push($(this).data('id'));
+    });
+
+    if (selected.length == 0) {
+        return;
+    }
+
+    if(confirm("{$confirm}")) {
+        $.post('/{$path}/' + selected.join(), {_method:'delete','_token':'{$token}'}, function(data){
+            $.pjax.reload('#pjax-container');
+            noty({
+                text: "<strong>Succeeded!</strong>",
+                type:'success',
+                timeout: 1000
+            });
+        });
+    }
+});
+
+$('.grid-refresh').on('click', function() {
+    $.pjax.reload('#pjax-container');
+
+    noty({
+        text: "<strong>Succeeded!</strong>",
+        type:'success',
+        timeout: 1000
+    });
+});
+
+var grid_order = function(id, direction) {
+    $.post('/{$path}/' + id, {_method:'PUT', _token:'{$token}', _orderable:direction}, function(data){
+
+        if (data.status) {
+            noty({
+                text: "<strong>Succeeded!</strong>",
+                type:'success',
+                timeout: 1000
+            });
+
+            $.pjax.reload('#pjax-container');
+        }
+    });
+}
+
+$('.grid-order-up').on('click', function() {
+    grid_order($(this).data('id'), 1);
+});
+
+$('.grid-order-down').on('click', function() {
+    grid_order($(this).data('id'), 0);
+});
+
+EOT;
+    }
+
+    /**
      * Get the string contents of the grid view.
      *
      * @return string
      */
     public function __toString()
     {
+        AdminManager::script($this->script());
+
         return $this->render();
     }
 }
