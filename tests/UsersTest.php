7bdc8bc826f7a353812a7bafe3861f2f0a5ade11
greateagle0303 <greateagle0303@gmail.com>
2016-11-21 16:02:25 +0800

    fix issue #113

--- tests/UsersTest.php
+++ tests/UsersTest.php
@@ -42,13 +42,22 @@ class UsersTest extends TestCase
             ->see('dashboard')
             ->seeIsAuthenticated('admin')
             ->seePageIs('admin');
+
+        $this->assertFalse($this->app['auth']->guard('admin')->getUser()->isAdministrator());
+
+        $this
+                ->dontSee('<span>Users</span>')
+                ->dontSee('<span>Roles</span>')
+                ->dontSee('<span>Permission</span>')
+                ->dontSee('<span>Operation log</span>')
+                ->dontSee('<span>Menu</span>');
     }
 
     public function testUpdateUser()
     {
         $this->visit('admin/auth/users/'.$this->user->id.'/edit')
             ->see('Create')
-            ->submitForm('Submit', ['name' => 'test'])
+            ->submitForm('Submit', ['name' => 'test', 'roles' => [1]])
             ->seePageIs('admin/auth/users')
             ->seeInDatabase(config('admin.database.users_table'), ['name' => 'test']);
     }
@@ -59,7 +68,7 @@ class UsersTest extends TestCase
 
         $this->visit('admin/auth/users/'.$this->user->id.'/edit')
             ->see('Create')
-            ->submitForm('Submit', ['password' => $password])
+            ->submitForm('Submit', ['password' => $password, 'roles' => [1]])
             ->seePageIs('admin/auth/users')
             ->visit('admin/auth/logout')
             ->dontSeeIsAuthenticated('admin')
