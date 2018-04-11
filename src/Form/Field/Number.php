426f4d10d58772237e3c13f8cabc21175c862038
greateagle0303 <greateagle0303@gmail.com>
2018-04-11 14:59:41 +0800

    fix bug for field rules of form used regex array

--- src/Form/Field/Number.php
+++ src/Form/Field/Number.php
@@ -28,30 +28,4 @@ EOT;
 
         return parent::render();
     }
-
-    /**
-     * Set min value of number field.
-     *
-     * @param integer $value
-     * @return $this
-     */
-    public function min($value)
-    {
-        $this->attribute('min', $value);
-
-        return $this;
-    }
-
-    /**
-     * Set max value of number field.
-     *
-     * @param integer $value
-     * @return $this
-     */
-    public function max($value)
-    {
-        $this->attribute('max', $value);
-
-        return $this;
-    }
 }
