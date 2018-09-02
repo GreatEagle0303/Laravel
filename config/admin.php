bb37637d85f7a9b7d358e931be5c4462cdeb59c0
greateagle0303 <greateagle0303@gmail.com>
2018-09-02 11:21:36 +0800

    improve: support multipleImage  in NestedForm

--- config/admin.php
+++ config/admin.php
@@ -272,7 +272,7 @@ return [
     | When you use command `php artisan admin:extend` to generate extensions,
     | the extension files will be generated in this directory.
     */
-    'extension_dir' => app_path('Admin/Extensions'),
+    'extension_dir' => admin_path('extensions'),
 
     /*
     |--------------------------------------------------------------------------
