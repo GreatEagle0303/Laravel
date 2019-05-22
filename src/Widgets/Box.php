16a208903c36ac88d5c186a23c80413570ff0a9e
greateagle0303 <greateagle0303@gmail.com>
2019-05-22 17:39:35 +0800

    Listbox support load remote options.

--- src/Widgets/Box.php
+++ src/Widgets/Box.php
@@ -3,7 +3,6 @@
 namespace Encore\Admin\Widgets;
 
 use Illuminate\Contracts\Support\Renderable;
-use Encore\Admin\Admin;
 
 class Box extends Widget implements Renderable
 {
@@ -26,11 +25,6 @@ class Box extends Widget implements Renderable
      * @var array
      */
     protected $tools = [];
-
-    /**
-     * @var string
-     */
-    protected $script;
 
     /**
      * Box constructor.
@@ -97,25 +91,6 @@ class Box extends Widget implements Renderable
     }
 
     /**
-     *  Set box body scrollable
-     *
-     * @param array $options
-     * @return $this
-     */
-    public function scrollable($options = [], $nodeSelector = '')
-    {
-        $this->id = uniqid('box-slim-scroll-');
-        $scrollOptions = json_encode($options);
-        $nodeSelector = $nodeSelector ?: '.box-body';
-
-        $this->script = <<<SCRIPT
-$("#{$this->id} {$nodeSelector}").slimScroll({$scrollOptions});
-SCRIPT;
-
-        return $this;
-    }
-
-    /**
      * Set box as removable.
      *
      * @return $this
@@ -172,7 +147,6 @@ SCRIPT;
             'content'    => $this->content,
             'tools'      => $this->tools,
             'attributes' => $this->formatAttributes(),
-            'script'     => $this->script,
         ];
     }
 
