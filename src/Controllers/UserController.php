b1e5e554f4985f8e1ec0cf75f7b10f20d994b1e2
greateagle0303 <greateagle0303@gmail.com>
2019-06-19 02:07:45 +0300

    Update Russian translate

--- src/Controllers/UserController.php
+++ src/Controllers/UserController.php
@@ -91,12 +91,11 @@ class UserController extends AdminController
         $form = new Form(new $userModel());
 
         $userTable = config('admin.database.users_table');
-        $connection = config('admin.database.connection');
 
         $form->display('id', 'ID');
         $form->text('username', trans('admin.username'))
-            ->creationRules(['required', "unique:{$connection}.{$userTable}"])
-            ->updateRules(['required', "unique:{$connection}.{$userTable},username,{{id}}"]);
+            ->creationRules(['required', "unique:{$userTable}"])
+            ->updateRules(['required', "unique:{$userTable},username,{{id}}"]);
 
         $form->text('name', trans('admin.name'))->rules('required');
         $form->image('avatar', trans('admin.avatar'));
