c530763ad7b4d30352699ccf2b3994bb4be08494
greateagle0303 <greateagle0303@gmail.com>
2019-11-08 15:40:27 +0800

    Update composer.json

--- tests/MenuTest.php
+++ tests/MenuTest.php
@@ -61,7 +61,7 @@ class MenuTest extends TestCase
 
     public function testShowPage()
     {
-        $this->visit('admin/auth/menu/1/edit')
+        $this->visit('admin/auth/menu/1')
             ->seePageIs('admin/auth/menu/1/edit');
     }
 
