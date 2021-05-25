2ec20436a39d3bc40a9a308dd195298a4239db43
greateagle0303 <greateagle0303@gmail.com>
2021-05-26 01:15:27 +0900

    Changed to a better fix by jxlwqq

--- src/Grid/Filter/Presenter/Text.php
+++ src/Grid/Filter/Presenter/Text.php
@@ -162,7 +162,7 @@ class Text extends Presenter
     {
         $options = json_encode($options);
 
-        $ids = (array)$this->filter->getId();
+        $ids = (array) $this->filter->getId();
 
         foreach ($ids as $id) {
             Admin::script("$('#{$this->filter->getFilterBoxId()} input.{$id}').inputmask($options);");
